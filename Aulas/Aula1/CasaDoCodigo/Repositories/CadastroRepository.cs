c05183ec12ddb5027531cd0745bf882b6e99c9f8
Denis Meneses <denis.meneses90@hotmail.com>
2024-08-25 07:23:16 -0300

    Add project files.

---
+++ Aulas/Aula1/CasaDoCodigo/Repositories/CadastroRepository.cs
@@ -0,0 +1,33 @@
+﻿using CasaDoCodigo.Models;
+using System;
+using System.Linq;
+
+namespace CasaDoCodigo.Repositories
+{
+    public interface ICadastroRepository
+    {
+        Cadastro Update(int cadastroId, Cadastro novocadastro);
+    }
+
+    public class CadastroRepository : BaseRepository<Cadastro>, ICadastroRepository
+    {
+        public CadastroRepository(ApplicationContext context) : base(context)
+        {
+        }
+
+        public Cadastro Update(int cadastroId, Cadastro novocadastro)
+        {
+            var cadastroDB = dbSet.Where(c => c.Id == cadastroId)
+                .FirstOrDefault();
+
+            if (cadastroDB == null)
+                throw new ArgumentNullException("cadastro");
+
+            cadastroDB.Update(novocadastro);
+            context.SaveChanges();
+
+            return cadastroDB;
+
+        }
+    }
+}
